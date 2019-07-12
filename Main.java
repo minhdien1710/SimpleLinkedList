b32944cc1448c993f0a68535b43488f8e51b7d98
minhdien1710 <48935148+minhdien1710@users.noreply.github.com>
2019-07-12 10:33:38 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList(10);
+        myLinkedList.addFirst(11);
+        myLinkedList.addFirst(12);
+        myLinkedList.addFirst(13);
+
+        myLinkedList.add(4,9);
+        myLinkedList.add(4,9);
+
+        myLinkedList.printList();
+
+
+    }
+}
